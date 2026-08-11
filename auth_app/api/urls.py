0073b536e602377ed42b9135b995a83018c1739b
marioramirez90 <mario-ramirez@hotmail.de>
2026-08-11 14:17:56 +0200

    Add login API endpoint with token authentication

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import RegistrationView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("registration/", RegistrationView.as_view(), name="registration"),
+    path("login/", obtain_auth_token, name="login"),
 ]
